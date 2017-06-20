f6de8742632fee0931133a0601ff12d81c6a0044
xesam <xesamguo@gmail.com>
2017-06-20 12:32:14 +0800

    module

--- app/src/main/java/io/github/xesam/pgyer/MainActivity.java
+++ app/src/main/java/io/github/xesam/pgyer/MainActivity.java
@@ -3,11 +3,14 @@ package io.github.xesam.pgyer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pgyersdk.update.PgyUpdateManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PgyUpdateManager.register(this, null);
     }
 }
---
+++ pgyer-module/src/main/java/io/github/xesam/pgyer/module/Pgyer.java
@@ -0,0 +1,14 @@
+package io.github.xesam.pgyer.module;
+
+/**
+ * Created by xesamguo@gmail.com on 17-6-20.
+ */
+
+public class Pgyer {
+    public void registerCrash() {
+
+    }
+
+    public void unregisterCrash() {
+    }
+}
